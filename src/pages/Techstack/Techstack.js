40d1d37d56389a4619cb2a408469622fd0124aac
Aryan Bansal <91452049+Bansal29@users.noreply.github.com>
2024-06-21 01:50:18 +0530

    build changes

--- src/pages/Techstack/Techstack.js
+++ src/pages/Techstack/Techstack.js
@@ -1,42 +1,3 @@
-// import React from "react";
-// import "./Techstack.css";
-// // import { rubberBand } from "react-awesome-reveal";
-// // import { Fade } from "react-awesome-reveal";
-// import { TechstackList } from "../../utils/TechstackList";
-// const Techstack = () => {
-//   return (
-//     <div>
-//       <div className="container techstack" id="techstack">
-//         <h2 className="col-12 mt-3 mb-1 text-center text-uppercase">
-//           Technologies stack
-//         </h2>
-//         <hr />
-//         <p className="pb-3 text-center">
-//           💻Including programming languages,frameworks,databases,front-end &
-//           back-end tools, and API's 💻
-//         </p>
-//         <div className="row">
-//           {TechstackList.map((tech) => (
-//             <div key={tech._id} className="col-md-3">
-//               <div className="card m-2">
-//                 <div className="card-content">
-//                   <div className="card-body">
-//                     <div className="media d-flex">
-//                       <tech.icon className="tech-icon" />
-//                       <h5>{tech.name}</h5>
-//                     </div>
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Techstack;
 import React, { useState, useEffect } from "react";
 import "./Techstack.css";
 import { TechstackList } from "../../utils/TechstackList";
@@ -47,7 +8,7 @@ const Techstack = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % TechstackList.length);
-    }, 3000); // Adjust interval time as needed
+    }, 3000);
 
     return () => clearInterval(interval);
   }, []);
